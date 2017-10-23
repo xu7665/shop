4046c9cf5142e071f24eba4329e35f132a947850
vincent <xu7665@gmail.com>
2017-10-23 17:58:55 +0800

    xadmin

--- Shop/settings.py
+++ Shop/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'users',
     'goods',
     'trade',
-    'user_operation'
+    'user_operation',
+    'xadmin',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -116,15 +118,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
